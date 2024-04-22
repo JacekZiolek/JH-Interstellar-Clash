c87dc8ca3e81663b89b473c6e0c2942e03389dae
Jack Herby <47577862+JackHerby@users.noreply.github.com>
2024-04-23 01:54:33 +0200

    feat: add basic layout for the app (#2)

--- app.vue
+++ app.vue
@@ -8,9 +8,41 @@ onMounted(() => console.log('app', someTest.value?.test))
 </script>
 
 <template>
-  <div>
-    <NuxtLayout>
+  <v-app>
+    <v-app-bar color="grey-lighten-2">
+      <template #prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+      <v-app-bar-title>Photos</v-app-bar-title>
+      <template #append>
+        <v-btn icon="mdi-dots-vertical"></v-btn>
+      </template>
+    </v-app-bar>
+    <v-main class="app-container">
       <NuxtPage />
-    </NuxtLayout>
-  </div>
+    </v-main>
+    <v-footer app>
+      <v-row align="center" no-gutters>
+        <v-col cols="auto">
+          <p class="text-no-wrap">
+            {{ new Date().getFullYear() }} — <strong>Jack Herby</strong>
+          </p>
+        </v-col>
+        <v-col cols="auto" class="pl-4">
+          <v-btn icon="mdi-github" class="mr-2" variant="text" />
+          <v-btn icon="mdi-linkedin" variant="text" />
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
+
+<style scoped>
+.app-container {
+  background-image: url('./public/space_battle.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
---
+++ components/SinglePlayer.vue
@@ -0,0 +1,9 @@
+<script lant="ts" setup>
+onMounted(() => console.log('single player'))
+</script>
+
+<template>
+  <div>
+    <h1>Single Player</h1>
+  </div>
+</template>
