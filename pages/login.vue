689ac758d2d8d73b2f6ac8fdae8c27d7f4b9133c
Jack Herby <JacekZiolek@proton.me>
2024-05-02 14:27:09 +0200

    feat: add log in and sign up forms validation

--- pages/login.vue
+++ pages/login.vue
@@ -7,6 +7,8 @@ definePageMeta({
 })
 const router = useRouter()
 
+const form = ref()
+
 const snackbar = ref(false)
 const timeout = ref(3000)
 const message = ref('')
@@ -20,6 +22,10 @@ const googleAuthProvider = new GoogleAuthProvider()
 const auth = useFirebaseAuth()!
 
 const handleSignIn = async (authMethod: EAuthMethod): Promise<void> => {
+  form.value.validate()
+  if (!form.value.isValid) {
+    return
+  }
   isLoading.value = true
   try {
     switch (authMethod) {
@@ -49,95 +55,99 @@ const handleSignIn = async (authMethod: EAuthMethod): Promise<void> => {
   <v-container class="fill-height">
     <v-row>
       <v-col>
-        <h4 class="text-h4 text-center my-8">Log In</h4>
-        <v-card
-          class="mx-auto pa-12 pb-8"
-          elevation="8"
-          max-width="448"
-          rounded="lg"
-          variant="tonal"
-          color="amber-darken-3"
-        >
-          <div class="text-subtitle-1 text-medium-emphasis">Username</div>
-          <v-text-field
-            v-model="email"
-            density="compact"
-            placeholder="Email address"
-            prepend-inner-icon="mdi-email-outline"
-            variant="outlined"
-          ></v-text-field>
-          <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-            Password
-            <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer" target="_blank">
-              Forgot login password?
-            </a>
-          </div>
-          <v-text-field
-            v-model="password"
-            :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
-            :type="isPasswordVisible ? 'text' : 'password'"
-            density="compact"
-            placeholder="Enter your password"
-            prepend-inner-icon="mdi-lock-outline"
-            variant="outlined"
-            @click:append-inner="isPasswordVisible = !isPasswordVisible"
-          ></v-text-field>
-          <v-btn
-            class="mb-8"
-            color="blue"
-            size="large"
+        <v-form ref="form">
+          <h4 class="text-h4 text-center my-8">Log In</h4>
+          <v-card
+            class="mx-auto pa-12 pb-8"
+            elevation="8"
+            max-width="448"
+            rounded="lg"
             variant="tonal"
-            block
-            :loading="isLoading"
-            :disabled="isLoading"
-            @click="handleSignIn(EAuthMethod.emailAndPassword)"
+            color="amber-darken-3"
           >
-            Log In
-          </v-btn>
-          <v-btn
-            class="mb-2"
-            size="large"
-            variant="flat"
-            block
-            :loading="isLoading"
-            :disabled="isLoading"
-            @click="handleSignIn(EAuthMethod.popUp)"
-          >
-            <v-icon class="mr-2" icon="mdi-google"></v-icon> Google
-          </v-btn>
-          <v-btn
-            class="mb-8"
-            size="large"
-            variant="flat"
-            block
-            :loading="isLoading"
-            :disabled="isLoading"
-            @click="handleSignIn(EAuthMethod.anonymously)"
-          >
-            <v-icon class="mr-2" icon="mdi-incognito"></v-icon> Anonymous User
-          </v-btn>
-          <v-card-text class="text-center">
-            <NuxtLink class="text-blue text-decoration-none" :to="{ name: 'signup' }" rel="noopener noreferrer">
-              Sign up now
-              <v-icon icon="mdi-chevron-right"></v-icon>
-            </NuxtLink>
-          </v-card-text>
-        </v-card>
-        <v-snackbar
-          v-model="snackbar"
-          :timeout="timeout"
-          color="red"
-          location="top"
-          vertical
-          multi-line
-        >
-          {{ message }}
-          <template #actions>
-            <v-btn color="blue" variant="text" @click="snackbar = false">
-              Close
+            <div class="text-subtitle-1 text-medium-emphasis">Username</div>
+            <v-text-field
+              v-model="email"
+              :rules="[v => !!v || `E-mail is required`]"
+              density="compact"
+              placeholder="Email address"
+              prepend-inner-icon="mdi-email-outline"
+              variant="outlined"
+            ></v-text-field>
+            <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+              Password
+              <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer" target="_blank">
+                Forgot login password?
+              </a>
+            </div>
+            <v-text-field
+              v-model="password"
+              :rules="[v => !!v || `Password is required`]"
+              :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+              :type="isPasswordVisible ? 'text' : 'password'"
+              density="compact"
+              placeholder="Enter your password"
+              prepend-inner-icon="mdi-lock-outline"
+              variant="outlined"
+              @click:append-inner="isPasswordVisible = !isPasswordVisible"
+            ></v-text-field>
+            <v-btn
+              class="mb-8"
+              color="blue"
+              size="large"
+              variant="tonal"
+              block
+              :loading="isLoading"
+              :disabled="isLoading"
+              @click="handleSignIn(EAuthMethod.emailAndPassword)"
+            >
+              Log In
             </v-btn>
-          </template>
-        </v-snackbar>
+            <v-btn
+              class="mb-2"
+              size="large"
+              variant="flat"
+              block
+              :loading="isLoading"
+              :disabled="isLoading"
+              @click="handleSignIn(EAuthMethod.popUp)"
+            >
+              <v-icon class="mr-2" icon="mdi-google"></v-icon> Google
+            </v-btn>
+            <v-btn
+              class="mb-8"
+              size="large"
+              variant="flat"
+              block
+              :loading="isLoading"
+              :disabled="isLoading"
+              @click="handleSignIn(EAuthMethod.anonymously)"
+            >
+              <v-icon class="mr-2" icon="mdi-incognito"></v-icon> Anonymous User
+            </v-btn>
+            <v-card-text class="text-center">
+              <NuxtLink class="text-blue text-decoration-none" :to="{ name: 'signup' }" rel="noopener noreferrer">
+                Sign up now
+                <v-icon icon="mdi-chevron-right"></v-icon>
+              </NuxtLink>
+            </v-card-text>
+          </v-card>
+          <v-snackbar
+            v-model="snackbar"
+            :timeout="timeout"
+            color="red"
+            location="top"
+            vertical
+            multi-line
+          >
+            {{ message }}
+            <template #actions>
+              <v-btn color="blue" variant="text" @click="snackbar = false">
+                Close
+              </v-btn>
+            </template>
+          </v-snackbar>
+        </v-form>
       </v-col>
     </v-row>
   </v-container>
