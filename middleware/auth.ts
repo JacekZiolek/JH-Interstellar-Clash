2bc4577bfe2bb33638444645c61636e533fc7ce9
Jack Herby <47577862+JackHerby@users.noreply.github.com>
2024-05-02 12:26:29 +0200

    fix: change redirect method in auth middleware (#16)

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,7 @@
 export default defineNuxtRouteMiddleware(async (to) => {
   const user = await getCurrentUser()
+  const router = useRouter()
   if (user && (to.name === 'login' || to.name === 'signup')) {
-    return navigateTo({ name: 'index' })
+    router.push({ name: 'index' })
   }
 })
