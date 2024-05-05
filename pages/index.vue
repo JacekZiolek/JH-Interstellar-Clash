64fee72af803354523c219f25084ac5faacd0a0a
Jack Herby <47577862+JackHerby@users.noreply.github.com>
2024-05-05 17:40:37 +0200

    refactor: add default nuxt layout (#23) (#24)
    Co-authored-by: Jacek Ziółek <jziolek@x-one.com.pl>

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,10 @@
 <script lang="ts" setup>
-import { useDisplay } from 'vuetify'
-
-const { smAndUp } = useDisplay()
 const isGameRunning = useGameRunning()
 </script>
 
 <template>
-  <v-container class="fill-height">
-    <v-row>
-      <v-spacer v-show="smAndUp"></v-spacer>
-      <v-col :cols="smAndUp? 10 : 12">
-        <v-card class="text-center px-2 py-4" variant="tonal" color="amber-darken-3">
-          <GameInstance v-if="isGameRunning" />
-          <GameMenu v-else />
-        </v-card>
-      </v-col>
-      <v-spacer v-show="smAndUp"></v-spacer>
-    </v-row>
-  </v-container>
+  <div>
+    <GameInstance v-if="isGameRunning" />
+    <GameMenu v-else />
+  </div>
 </template>
